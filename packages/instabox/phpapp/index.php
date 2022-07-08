d9b83f29660da0302973d99bd8b2b91a8c5e4719
techswarn <tech.swarns@gmail.com>
2022-07-08 08:52:12 +0530

    code update

--- packages/instabox/phpapp/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-  function main(array $args) : array
-  {
-      $name = $args["name"] ?? "stranger";
-      $greeting = "Hello $name!";
-      echo $greeting;
-      return ["body" => $greeting];
-  }
-  
