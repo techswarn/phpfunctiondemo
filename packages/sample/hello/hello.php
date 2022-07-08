9549bdec6ad7ddfbaa582cc19ed4d4c9f4163072
techswarn <tech.swarns@gmail.com>
2022-07-08 08:58:44 +0530

    code update

--- packages/sample/hello/hello.php
+++ packages/sample/hello/hello.php
@@ -1,9 +1,14 @@
 <?php
-  function main(array $args) : array
-  {
-      $name = $args["name"] ?? "stranger";
-      $greeting = "Hello $name!";
-      echo $greeting;
-      return ["body" => $greeting];
-  }
-  
+use KubAT\PhpSimple\HtmlDomParser;
+
+function main(array $args) : string {
+
+    $instaboxHTML = HtmlDomParser::str_get_html(file_get_contents($args['instaURL']));
+
+    foreach ($instaboxHTML->find('track-trace') as $trackTrace) {
+        $shipmentJson = htmlspecialchars_decode($trackTrace->attr[':shipment']);
+    }
+
+    return $shipmentJson;
+
+}
